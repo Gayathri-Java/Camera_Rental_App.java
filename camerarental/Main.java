271bc5c6a505b7f64b4335b698cf94ed520c0a4e
gayathri ethamukkala <egayathri20122000@gmail.com>
2023-11-02 14:07:42 +0530

    Camera Rental

---
+++ camerarental/Main.java
@@ -0,0 +1,10 @@
+package com.camerarental;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CameraRentalApp cameraRentalApp = new CameraRentalApp();
+				 cameraRentalApp.displayWelcomeScreen();
+				 cameraRentalApp.login();
+				 }
+				}
